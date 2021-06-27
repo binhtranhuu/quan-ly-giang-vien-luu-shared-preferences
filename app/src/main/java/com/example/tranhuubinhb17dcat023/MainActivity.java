2469b561325ba03bddb90bb7eb45b1e06965a97c
TRAN-HUU-BINH\tranb <tranbinh241999@gmail.com>
2021-06-27 09:45:49 +0700

    2. Thiết kế giao diện trang chủ

--- app/src/main/java/com/example/tranhuubinhb17dcat023/MainActivity.java
+++ app/src/main/java/com/example/tranhuubinhb17dcat023/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.tranhuubinhb17dcat023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnGiangVien, btnChuyenMon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnGiangVien = findViewById(R.id.btnGiangVien);
+        btnChuyenMon = findViewById(R.id.btnChuyenMon);
+
+        btnGiangVien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ManGiangVien.class);
+                startActivity(intent);
+            }
+        });
+
+        btnChuyenMon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ManChuyenMon.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/tranhuubinhb17dcat023/ManAddGiangVien.java
@@ -0,0 +1,14 @@
+package com.example.tranhuubinhb17dcat023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManAddGiangVien extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_add_giang_vien);
+    }
+}
